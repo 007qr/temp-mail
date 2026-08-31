ee94702ba73cfc3f7814bda5b5b094b55b535853
007qr <ayush@007qr.dev>
2026-08-31 21:14:05 +0530

    feat: sitemap and robots file

--- app/inbox/[alias]/page.tsx
+++ app/inbox/[alias]/page.tsx
@@ -1,4 +1,12 @@
 import { Inbox } from "@/components/inbox"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false
+  }
+}
 
 export default async function Page({ params }: { params: Promise<{ alias: string }> }) {
   const { alias } = await params
